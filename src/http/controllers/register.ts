d24e9608297ad2222a519a385ec76e475b0f2542
Caio Vinícius <caio1525pereira@gmail.com>
2023-03-23 22:09:12 -0300

    feat: :lock: Hash da senha do usuário
    Caso de Uso e Design Patterns - Hash da senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from "fastify";
+import { hash } from "bcryptjs";
 import { z } from "zod";
 
 import { prisma } from "@/lib/prisma";
@@ -12,11 +13,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
+  const passwordHash = await hash(password, 6);
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password
+      password_hash: passwordHash
     }
   });
 
