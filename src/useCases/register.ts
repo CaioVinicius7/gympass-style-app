02e0abb1827203798fe129be5ae3aca4d759b30e
Caio Vinícius <caio1525pereira@gmail.com>
2023-04-04 22:28:58 -0300

    feat: :sparkles: Adicionando retorno ao caso de uso de criação de usuário
    Design Patterns & Testes - Primeiro teste unitário

--- src/useCases/register.ts
+++ src/useCases/register.ts
@@ -3,6 +3,7 @@ import { hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 import { usersRepository } from "@/repositories/users-repository";
+import type { UserDTO } from "@/dtos/user-dto";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -10,10 +11,18 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
+interface RegisterUseCaseResponse {
+  user: UserDTO;
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: usersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
 
     if (userWithSameEmail) {
@@ -22,10 +31,14 @@ export class RegisterUseCase {
 
     const passwordHash = await hash(password, 6);
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash: passwordHash
     });
+
+    return {
+      user
+    };
   }
 }
