dc11bf0f855d8568fea338d657885bc9abf3f0d2
Caio Vinícius <caio1525pereira@gmail.com>
2023-04-23 19:26:19 -0300

    feat: :sparkles: Método countByUserId do repositório de check ins
    Implementando casos de uso - Caso de uso de métricas

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -5,5 +5,6 @@ import type { CreateCheckInPayload } from "./payloads";
 export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
+  countByUserId(userId: string): Promise<number>;
   create(data: CreateCheckInPayload): Promise<CheckIn>;
 }
