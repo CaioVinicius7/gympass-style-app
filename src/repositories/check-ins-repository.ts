16238937eecc65a5fbf483f5e432987f28f6826b
Caio Vinícius <caio1525pereira@gmail.com>
2023-04-23 19:01:37 -0300

    feat: :sparkles: Método findManyByUserId no repositório de check ins
    Implementando casos de uso - Caso de uso de histórico

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,5 +4,6 @@ import type { CreateCheckInPayload } from "./payloads";
 
 export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
+  findManyByUserId(userId: string): Promise<CheckIn[]>;
   create(data: CreateCheckInPayload): Promise<CheckIn>;
 }
--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -16,13 +16,13 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     const startOfTheDay = dayjs(date).startOf("date");
     const endOfTheDay = dayjs(date).endOf("date");
 
-    const checkOnSameDate = this.items.find((checkIn) => {
-      const checkInDate = dayjs(checkIn.created_at);
+    const checkOnSameDate = this.items.find((item) => {
+      const checkInDate = dayjs(item.created_at);
 
       const isOnSameDate =
         checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
 
-      return checkIn.user_id === userId && isOnSameDate;
+      return item.user_id === userId && isOnSameDate;
     });
 
     if (!checkOnSameDate) {
@@ -32,11 +32,17 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkOnSameDate;
   }
 
+  async findManyByUserId(userId: string): Promise<CheckIn[]> {
+    const checkIns = this.items.filter((item) => item.user_id === userId);
+
+    return checkIns;
+  }
+
   async create(data: CreateCheckInPayload): Promise<CheckIn> {
     const checkIn = {
       id: randomUUID(),
       user_id: data.user_id,
-      gym_id: data.user_id,
+      gym_id: data.gym_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
       created_at: new Date()
     };
