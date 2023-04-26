93c3f02ab0be90785a0cf534e6c1540b33bfe0b6
Caio Vinícius <caio1525pereira@gmail.com>
2023-04-26 20:46:48 -0300

    refactor: :recycle: Organizando imports do repositório de usuários do prisma
    Controllers & Testes E2E - Repositórios do Prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,9 +1,9 @@
-import { usersRepository } from "../users-repository";
-
-import { prisma } from "@/lib/prisma";
 import type { User } from "@/types";
 
-import type { CreateUserPayload } from "../payloads/create-user-payload";
+import { prisma } from "@/lib/prisma";
+
+import type { usersRepository } from "../users-repository";
+import type { CreateUserPayload } from "../payloads";
 
 export class PrismaUsersRepository implements usersRepository {
   async findById(id: string): Promise<User | null> {
