98e55b0019c4f4e60594c8f45d6891d002723a54
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-04 23:08:59 -0300

    test: :white_check_mark: Teste E2E da criação de usuários
    Controllers & Testes E2E - Teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,24 @@
-import { it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
 
-it("Test", () => {});
+import { app } from "@/app";
+
+describe("Register (E2E)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("Should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456"
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
