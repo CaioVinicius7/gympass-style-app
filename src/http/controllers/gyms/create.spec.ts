b2f8c6227eb6d7b0b9bdfcee3e877335e4d63451
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-08 21:23:07 -0300

    test: :white_check_mark: Testes E2E da criação de academia
    Controllers & Testes E2E - Testes E2E de rotas de academias

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,32 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+
+import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Create Gym (E2E)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("Should be able to create a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "JavaScript Gym",
+        description: "Some description.",
+        phone: "(99) 99999-9999",
+        latitude: -22.4173682,
+        longitude: -45.4869312
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
