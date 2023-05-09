06ef8f1b882a4d9f46d2365975d933600d96d842
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-08 21:21:51 -0300

    fix: :bug: Corrigindo endpoint de validação da criação de academia
    Controllers & Testes E2E - Testes E2E de rotas de academias

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -12,5 +12,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get("/gyms/search", search);
   app.get("/gyms/nearby", fetchNearby);
 
-  app.post("gyms", create);
+  app.post("/gyms", create);
 }
