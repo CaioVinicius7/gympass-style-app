600402c65cfa98d31c7d694dd177a1aa37456bdc
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-08 21:22:17 -0300

    feat: :sparkles: Adicionando rotas de check ins
    Controllers & Testes E2E - Testes E2E de rotas de academias

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { ZodError } from "zod";
 import { env } from "./env";
 import { userRoutes } from "./http/controllers/users/routes";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 
 const app = fastify();
 
@@ -14,6 +15,7 @@ app.register(fastifyJwt, {
 
 app.register(userRoutes);
 app.register(gymsRoutes);
+app.register(checkInsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
